234f52575ad0ee2c18a708be18c081ebfe588fe5
bigalr8 <bigalr8@icloud.com>
2019-05-02 19:25:18 -0400

    added server code, routes and data

---
+++ app/data/friendsData.js
@@ -0,0 +1,45 @@
+// ===============================================================================
+ 
+
+
+
+
+ 
+
+
+var friendsArray = [
+    {
+      name: "Alpha Betz",
+      photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMLdpgrHTNGI_DEOTzf3VYjdDXpmnytyl7VGFSlJpGZGblaJ-9vg",
+      scores: [3,2,1,5,4,3,5,4,6,2]
+    },
+    {
+        name: "Beau Brummel",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSowDZT2gje9oYTe_Nhix0nHRotD6M2hu3kpVp6wH55_qvTG7R5",
+        scores: [3,2,1,5,4,3,5,4,1,2]
+    },
+    {
+        name: "Carazy Catt",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRYRkjZgtgRMyF0sT49ApgzgYNEEsXdD817wIa3QvSJnyqskQM",
+        scores: [5,3,2,5,4,2,4,1,2,4]
+    },
+    {
+        name: "Dowin Durdy",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUThY1vLIcRBh8lnzEs83oT97s6bmsIg0UaXr1MToE2FL9D-juOA",
+        scores: [4,3,5,3,4,2,3,2,1,2]
+    },
+    {
+        name: "Elizabeth Boupe",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcXSDlNVvC3xoh1A2w0EdcqNjHwSJuTI2Wr-rpmMaguSCrwp5USw",
+        scores: [3,4,3,1,3,2,4,5,1,4]
+    },
+
+
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendsArray;
+  
+
+
+    
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,62 @@
+//= apiRoutes.js API Routes
+
+var friendData = require("../data/friendsData");
+var lowScore = 0;
+var lowIndex = 0;
+var compArray = [];
+var newArray = [];
+var result = 0;
+var cmptblty =0;
+   
+  module.exports = function(app) {
+
+    app.get("/api/friends", function(req, res) {
+      return res.json(friendData);
+    }); //end get
+      
+    app.post("/api/friends", function(req, res) {
+       
+      var newfriend = req.body;
+           
+      newfriend.routeName = newfriend.name.replace(/\s+/g, "").toLowerCase();
+    
+      friendData.push(newfriend);
+    
+      newArray = newfriend.scores;
+             
+      for (i=0;i<friendData.length;i++) {
+          if(friendData[i] !== newfriend){
+            compArray = friendData[i].scores;
+            compare();
+          }
+        } //end for
+              
+      res.json(friendData[lowIndex]);
+    });//end post
+
+    function compare() {
+    cmptblty = 0;
+      for (j=0;j<compArray.length;j++) {
+           
+          if (compArray[j] !== newArray[j]) {
+              result = compArray[j] - newArray[j];
+              if (result < 0) {
+                  result = result * -1;
+              } // end if
+              cmptblty = cmptblty + result;
+          } // end outer if 
+      }// end for
+      
+      if (i == 0){
+          lowScore = cmptblty;
+          lowIndex = i;
+      } // end if
+      if (cmptblty < lowScore){
+          lowScore = cmptblty;
+          lowIndex = i;
+      } //end if
+    }  
+
+}//end function
+
+    
